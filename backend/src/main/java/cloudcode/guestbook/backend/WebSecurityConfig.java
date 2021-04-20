8b1d68015b91f4e66ee3563970cf8af726115125
Elijah Yap <yap@usc.edu>
2021-04-20 15:51:46 -0700

    try to permit all connections on backend

--- backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
+++ backend/src/main/java/cloudcode/guestbook/backend/WebSecurityConfig.java
@@ -17,18 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-      .antMatchers("/", "/home")
-      .permitAll()
-      .antMatchers("/css/**")
-      .permitAll()
-      .anyRequest()
-      .authenticated()
-      .and()
-      .formLogin()
-      .loginPage("/login")
-      .permitAll()
-      .and()
-      .logout()
+      .antMatchers("/**")
       .permitAll();
   }
 
