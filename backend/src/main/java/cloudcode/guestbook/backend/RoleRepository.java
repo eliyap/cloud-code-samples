4011244c82425257cf861e38a3c8dd5edcfda6c7
Elijah Yap <yap@usc.edu>
2021-04-20 15:36:06 -0700

    Revert "add security classes"
    This reverts commit 8da0ea5345a936a44231482131bb096bd83c2676.

--- backend/src/main/java/cloudcode/guestbook/backend/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package cloudcode.guestbook.backend;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-  Role findByRole(String role);
-}
--- backend/src/main/java/cloudcode/guestbook/backend/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package cloudcode.guestbook.backend;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-  User findByEmail(String email);
-}
