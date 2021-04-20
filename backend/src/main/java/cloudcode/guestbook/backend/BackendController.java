d00ccf1263c404865d283e43f9b0cad53c40a918
Elijah Yap <yap@usc.edu>
2021-04-20 16:11:53 -0700

    add backend endpoint for posting user

--- backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
+++ backend/src/main/java/cloudcode/guestbook/backend/BackendController.java
@@ -1,13 +1,18 @@
 package cloudcode.guestbook.backend;
 
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * defines the REST endpoints managed by the server.
@@ -55,9 +60,40 @@ public class BackendController {
     repository.save(message);
   }
 
+//   @PostMapping("/signup")
+//   public final void addUser(@RequestBody GuestBookEntry message) {
+//     message.setDate(System.currentTimeMillis());
+//     repository.save(message);
+//   }
+
+  @Autowired
+  CustomUserDetailsService userService;
+
   @PostMapping("/signup")
-  public final void addUser(@RequestBody GuestBookEntry message) {
-    message.setDate(System.currentTimeMillis());
-    repository.save(message);
+  public ModelAndView createNewUser(
+    @Valid User user,
+    BindingResult bindingResult
+  ) {
+    ModelAndView modelAndView = new ModelAndView();
+    User userExists = userService.findUserByEmail(user.getEmail());
+    if (userExists != null) {
+      bindingResult.rejectValue(
+        "email",
+        "error.user",
+        "There is already a user registered with the username provided"
+      );
+    }
+    if (bindingResult.hasErrors()) {
+      modelAndView.setViewName("signup");
+    } else {
+      userService.saveUser(user);
+      modelAndView.addObject(
+        "successMessage",
+        "User has been registered successfully"
+      );
+      modelAndView.addObject("user", new User());
+      modelAndView.setViewName("login");
+    }
+    return modelAndView;
   }
 }
