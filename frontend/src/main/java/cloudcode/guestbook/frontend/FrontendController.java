e8a513c0b229f65790bd769175b9097a1a54055f
Elijah Yap <yap@usc.edu>
2021-04-20 17:15:26 -0700

    Revert "change to postmappings"
    This reverts commit c6e36cb0988e3056d44925fb65f2eafce79ca2b2.

--- frontend/src/main/java/cloudcode/guestbook/frontend/FrontendController.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/FrontendController.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
@@ -88,7 +89,7 @@ public class FrontendController {
    * @return redirects back to home page
    * @throws URISyntaxException when there is an issue with the backend uri
    */
-  @PostMapping("/signup")
+  @RequestMapping(value = "/signup", method = RequestMethod.POST)
   public final String post(final GuestBookEntry formMessage)
     throws URISyntaxException {
     URI url = new URI(signupUri);
@@ -111,7 +112,7 @@ public class FrontendController {
    * @return redirects back to home page
    * @throws URISyntaxException when there is an issue with the backend uri
    */
-  @PostMapping("/query")
+  @RequestMapping(value = "/query", method = RequestMethod.POST)
   public final String query(final GuestBookEntry formMessage)
     throws URISyntaxException {
     URI url = new URI(signupUri);
