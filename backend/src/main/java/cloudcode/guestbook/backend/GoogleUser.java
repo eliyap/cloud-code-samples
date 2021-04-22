4a46006e8edd6c79891f0509da7f40163b8497f0
Elijah Yap <yap@usc.edu>
2021-04-22 15:48:25 -0700

    Update GoogleUser.java

--- backend/src/main/java/cloudcode/guestbook/backend/GoogleUser.java
+++ backend/src/main/java/cloudcode/guestbook/backend/GoogleUser.java
@@ -14,6 +14,8 @@ public class GoogleUser {
   )
   private String email;
 
+  private String id_token;
+
   public GoogleUser() {}
 
   public final String getEmail() {
@@ -22,5 +24,13 @@ public class GoogleUser {
 
   public final void setEmail(String email) {
     this.email = email;
+  }
+  
+  public final String getIdToken() {
+    return id_token;
+  }
+
+  public final void setIdToken(String id_token) {
+    this.id_token = id_token;
   }
 }
