b6d7c1c54ed23d4c18a188312b1ef36490db2438
Elijah Yap <yap@usc.edu>
2021-04-22 18:16:36 -0700

    clone manual system over to google auth

--- frontend/src/main/java/cloudcode/guestbook/frontend/GoogleUserLoginController.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/GoogleUserLoginController.java
@@ -3,11 +3,16 @@ package cloudcode.guestbook.frontend;
 import java.net.URI;
 import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +25,9 @@ public class GoogleUserLoginController {
 
   @Autowired
   protected AuthenticationManager authenticationManager;
+
+  @Autowired
+  protected GoogleUserAuthenticationProvider googleUserAuthenticationProvider;
 
   @PostMapping(
     value = "/googlesignin",
@@ -41,8 +49,27 @@ public class GoogleUserLoginController {
       );
 
     // try manual auth
-    // login(request, "foo", "bar");
+    login(request, "foo", "bar");
 
     return "redirect:/";
+  }
+
+  // Manually log the User in.
+  // Source: https://stackoverflow.com/a/8336233/12395667
+  public void login(
+    HttpServletRequest request,
+    String userName,
+    String password
+  ) {
+    // Authenticate the user
+    Authentication authentication = googleUserAuthenticationProvider.authenticate(
+      new UsernamePasswordAuthenticationToken(userName, password)
+    );
+    SecurityContext securityContext = SecurityContextHolder.getContext();
+    securityContext.setAuthentication(authentication);
+
+    // Create a new session and add the security context.
+    HttpSession session = request.getSession(true);
+    session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
   }
 }
