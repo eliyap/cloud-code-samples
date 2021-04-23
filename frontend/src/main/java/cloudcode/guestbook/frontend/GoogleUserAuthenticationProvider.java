61c58cc6d0004a88152954f070f3df6afd56bf4e
Elijah Yap <yap@usc.edu>
2021-04-22 18:06:17 -0700

    clone Auth Provider

---
+++ frontend/src/main/java/cloudcode/guestbook/frontend/GoogleUserAuthenticationProvider.java
@@ -0,0 +1,50 @@
+package cloudcode.guestbook.frontend;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.springframework.http.HttpEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GoogleUserAuthenticationProvider implements AuthenticationProvider {
+
+  @Override
+  public Authentication authenticate(Authentication authentication)
+    throws AuthenticationException {
+    String username = authentication.getPrincipal().toString();
+    String password = authentication.getCredentials().toString();
+    URI url;
+
+    try {
+      url = new URI(BackendURI.LOGIN);
+    } catch (URISyntaxException e) {
+      throw new AuthenticationServiceException(
+        "Could not construct backend URL!"
+      );
+    }
+
+    UserResponse result = new RestTemplate()
+    .postForObject(
+        url,
+        new HttpEntity<User>(new User("", username, password)),
+        UserResponse.class
+      );
+    if (result.success) {
+      return new UsernamePasswordAuthenticationToken(username, password);
+    } else {
+      throw new BadCredentialsException(result.errorMessage);
+    }
+  }
+
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return authentication.equals(UsernamePasswordAuthenticationToken.class);
+  }
+}
