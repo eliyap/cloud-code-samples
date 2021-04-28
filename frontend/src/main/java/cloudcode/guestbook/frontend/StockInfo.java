2d0bab6fa15377fde27b1a044bc0b6e185285bba
Elijah Yap <yap@usc.edu>
2021-04-28 13:21:55 -0700

    shim in IEX

--- frontend/src/main/java/cloudcode/guestbook/frontend/StockInfo.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/StockInfo.java
@@ -2,14 +2,24 @@ package cloudcode.guestbook.frontend;
 
 public class StockInfo {
 
-  public StockDetails price;
   public StockMeta metadata;
+
+  // only returned for non-authenticated users
+  public StockDetails price;
+
+  // only returned for authenticated users
+  public StockIEX iex;
 
   // No Args Constructor for Jackson
   public StockInfo() {}
 
   public StockInfo(StockDetails price, StockMeta metadata) {
     this.price = price;
+    this.metadata = metadata;
+  }
+  
+  public StockInfo(StockIEX iex, StockMeta metadata) {
+    this.iex = iex;
     this.metadata = metadata;
   }
 }
