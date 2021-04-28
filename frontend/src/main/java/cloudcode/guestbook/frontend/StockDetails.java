e0d216a6152de523637a24ecba63a6aefd43ffb1
Elijah Yap <yap@usc.edu>
2021-04-27 20:12:44 -0700

    Update StockDetails.java

--- frontend/src/main/java/cloudcode/guestbook/frontend/StockDetails.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/StockDetails.java
@@ -3,6 +3,10 @@ package cloudcode.guestbook.frontend;
 public class StockDetails {
 
   public Float low;
+  public Float high;
+  public Float open;
+  public Float close;
+  public Integer volume;
 
   // No Args Constructor for Jackson
   public StockDetails() {}
