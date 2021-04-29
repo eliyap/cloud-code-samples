cf9a905df83143f995e74803051356661bd2f1e8
Elijah Yap <yap@usc.edu>
2021-04-28 18:21:58 -0700

    add an endpoint to check for favorites being persisted

--- backend/src/main/java/cloudcode/guestbook/backend/FavoriteController.java
+++ backend/src/main/java/cloudcode/guestbook/backend/FavoriteController.java
@@ -2,6 +2,8 @@ package cloudcode.guestbook.backend;
 
 import java.net.URISyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,13 +32,30 @@ public class FavoriteController {
     User user = userRepository.findByEmail(email);
 
     if (googleUser != null) {
-      //TODO: thing
+      googleUser.getFavorites().add(ticker);
       googleUserRepository.save(googleUser);
     } else if (user != null) {
-      //TODO: thing
+      user.getFavorites().add(ticker);
       userRepository.save(user);
     } else {
       System.err.println("No User Found with Email: " + email);
+    }
+  }
+
+  @GetMapping("/favorites")
+  final ResponseEntity<?> favorites(
+    @RequestParam String email,
+    @RequestParam String ticker
+  ) {
+    GoogleUser googleUser = googleUserRepository.findByEmail(email);
+    User user = userRepository.findByEmail(email);
+    if (googleUser != null) {
+      return ResponseEntity.ok(googleUser.getFavorites().contains(ticker));
+    } else if (user != null) {
+      return ResponseEntity.ok(user.getFavorites().contains(ticker));
+    } else {
+      System.err.println("(Favorites) No User Found with Email: " + email);
+      return ResponseEntity.status(403).body("No User Found with Email");
     }
   }
 }
