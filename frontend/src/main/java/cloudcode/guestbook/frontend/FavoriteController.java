d962c43f13fcb062d6a4aee1061a5eee58cb573f
Elijah Yap <yap@usc.edu>
2021-04-28 17:24:48 -0700

    forward to backend

--- frontend/src/main/java/cloudcode/guestbook/frontend/FavoriteController.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/FavoriteController.java
@@ -1,8 +1,11 @@
 package cloudcode.guestbook.frontend;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class FavoriteController {
@@ -11,8 +14,14 @@ public class FavoriteController {
   final void favorite(
     @RequestParam String ticker,
     @RequestParam boolean favorite
-  ) {
+  )
+    throws URISyntaxException {
     System.out.println(ticker);
     System.out.println(favorite);
+    RestTemplate template = new RestTemplate();
+    URI url = new URI(
+      BackendURI.FAVORITE + "?ticker=" + ticker + "&favorite=" + favorite
+    );
+    template.postForLocation(url, null);
   }
 }
