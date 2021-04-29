e9fce57d9a89e52be9e8bfccd1de422d5192bb30
Elijah Yap <yap@usc.edu>
2021-04-28 20:45:35 -0700

    populate template with iex data

--- frontend/src/main/java/cloudcode/guestbook/frontend/FavoriteController.java
+++ frontend/src/main/java/cloudcode/guestbook/frontend/FavoriteController.java
@@ -2,7 +2,10 @@ package cloudcode.guestbook.frontend;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +35,18 @@ public class FavoriteController {
         .toString()
     );
     template.postForLocation(url, null);
+  }
+
+  @GetMapping("/favorites")
+  final String favorites(Model model) throws URISyntaxException {
+    RestTemplate template = new RestTemplate();
+    URI url = new URI(BackendURI.FAVORITES);
+    TickerList list = template.getForEntity(url, TickerList.class).getBody();
+    ArrayList<StockIEX> iexList = new ArrayList<StockIEX>();
+    for (String ticker : list) {
+      iexList.add(StockFetch.fetchIEX(ticker));
+    }
+    model.addAttribute("stocks", iexList);
+    return "favorites";
   }
 }
