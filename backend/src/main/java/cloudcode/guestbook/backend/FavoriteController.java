b6ec0693048019cd368e2931275df012def0537c
Elijah Yap <yap@usc.edu>
2021-04-28 20:31:50 -0700

    define endpoint, simplify some code

--- backend/src/main/java/cloudcode/guestbook/backend/FavoriteController.java
+++ backend/src/main/java/cloudcode/guestbook/backend/FavoriteController.java
@@ -17,6 +17,14 @@ public class FavoriteController {
   @Autowired
   private UserRepository userRepository;
 
+  /**
+   * Allows user with specified email to add or remove the
+   * stock with specified ticker from their favorites list
+   * @param ticker
+   * @param favorite
+   * @param email
+   * @throws URISyntaxException
+   */
   @PostMapping("/favorite")
   final void favorite(
     @RequestParam String ticker,
@@ -24,25 +32,23 @@ public class FavoriteController {
     @RequestParam String email
   )
     throws URISyntaxException {
-    System.out.println(ticker);
-    System.out.println(favorite);
-    System.out.println("Emial: " + email);
+    ticker = ticker.toUpperCase();
 
     GoogleUser googleUser = googleUserRepository.findByEmail(email);
     User user = userRepository.findByEmail(email);
 
     if (googleUser != null) {
       if (favorite) {
-        googleUser.getFavorites().add(ticker.toUpperCase());
+        googleUser.getFavorites().add(ticker);
       } else {
-        googleUser.getFavorites().remove(ticker.toUpperCase());
+        googleUser.getFavorites().remove(ticker);
       }
       googleUserRepository.save(googleUser);
     } else if (user != null) {
       if (favorite) {
-        user.getFavorites().add(ticker.toUpperCase());
+        user.getFavorites().add(ticker);
       } else {
-        user.getFavorites().remove(ticker.toUpperCase());
+        user.getFavorites().remove(ticker);
       }
       userRepository.save(user);
     } else {
@@ -50,7 +56,40 @@ public class FavoriteController {
     }
   }
 
+  /**
+   * Checks whether the user with the given email
+   * listed the given ticker as a favorite
+   * @param email
+   * @param ticker
+   * @return true if user did set this as a ticker
+   */
   @GetMapping("/checkfavorite")
+  final ResponseEntity<?> checkfavorite(
+    @RequestParam String email,
+    @RequestParam String ticker
+  ) {
+    ticker = ticker.toUpperCase();
+
+    GoogleUser googleUser = googleUserRepository.findByEmail(email);
+    User user = userRepository.findByEmail(email);
+
+    if (googleUser != null) {
+      return ResponseEntity.ok(googleUser.getFavorites().contains(ticker));
+    } else if (user != null) {
+      return ResponseEntity.ok(user.getFavorites().contains(ticker));
+    } else {
+      System.err.println("(Favorites) No User Found with Email: " + email);
+      return ResponseEntity.status(403).body("No User Found with Email");
+    }
+  }
+
+  /**
+   * Returns a list of the ticker symbols of the user's favorite stocks
+   * @param email
+   * @param ticker
+   * @return
+   */
+  @GetMapping("/favorites")
   final ResponseEntity<?> favorites(
     @RequestParam String email,
     @RequestParam String ticker
@@ -58,13 +97,9 @@ public class FavoriteController {
     GoogleUser googleUser = googleUserRepository.findByEmail(email);
     User user = userRepository.findByEmail(email);
     if (googleUser != null) {
-      return ResponseEntity.ok(
-        googleUser.getFavorites().contains(ticker.toUpperCase())
-      );
+      return ResponseEntity.ok(googleUser.getFavorites());
     } else if (user != null) {
-      return ResponseEntity.ok(
-        user.getFavorites().contains(ticker.toUpperCase())
-      );
+      return ResponseEntity.ok(user.getFavorites());
     } else {
       System.err.println("(Favorites) No User Found with Email: " + email);
       return ResponseEntity.status(403).body("No User Found with Email");
